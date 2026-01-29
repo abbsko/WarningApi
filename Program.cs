d5a94d20110d92ff14a1dad697a0c6ef3a4f4653
Skold <abbesk02@gmail.com>
2026-01-29 10:36:20 +0100

    Add Setup warning

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.MapControllers();
+
+app.MapGet("/warning", () => "Warning!")
+   .WithName("GetWarning");
+
+app.Run();
